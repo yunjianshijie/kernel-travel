1f0ebc07a0cdc68a10f5f7de4080664c9a473516
yunjianshijie <dailingjia@uniontech.com>
2025-08-19 11:32:28 +0800

    slab初始化

---
+++ include/xkernel/slab.h
@@ -0,0 +1,43 @@
+#ifndef __LINUX_SLAB_H__
+#define __LINUX_SLAB_H__
+
+#include <xkernel/kernel.h>
+#include <xkernel/init.h>
+#include <xkernel/list.h>
+#include <xkernel/types.h>
+
+#include <asm/numa.h>
+
+struct kmem_cache {
+    
+    size_t object_size;
+
+    char name[1024];
+
+    /* 伪 numn结构，只有一个numu结点 */
+    struct list_head slabs_empty;
+    struct list_head slabs_partial;
+    struct list_head slabs_full;
+
+    // struct kmem_cache_node *node[MAX_NUMNODES];
+
+};
+
+struct slab {
+    //反指kmem_cache 
+    struct kmem_cache * slab_cache;
+
+    struct list_head slab_list;
+	void *freelist;	/* array of free object indexes */
+	void *s_mem;	/* first object */
+
+    int inuse; /*使用计数*/
+};
+
+
+
+void *kmem_cache_alloc(struct kmem_cache *cachep, gfp_t flags);
+void kmem_cache_free(struct kmem_cache *s, void *objp);
+
+
+#endif
---
+++ mm/slab.c
@@ -0,0 +1,6 @@
+#include <xkernel/kernel.h>
+#include <xkernel/init.h>
+#include <xkernel/slab.h>
+#include <xkernel/memory.h>
+
+
